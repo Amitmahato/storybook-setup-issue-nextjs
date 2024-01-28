8bc10760b4a0460a930f09c9bb5f973c85f26474
Amit Mahato <amit@progressivelabs.tech>
2024-01-28 16:39:41 +0545

    add ag-grid-react to reproducible repp

---
+++ src/app/grid-example.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { AgGridReact } from "ag-grid-react"; // React Grid Logic
+import "ag-grid-community/styles/ag-grid.css"; // Core CSS
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
+import { useState } from "react";
+
+export const GridExample = () => {
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ]);
+
+  return (
+    <div className="ag-theme-quartz" style={{ height: "100vh" }}>
+      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+    </div>
+  );
+};
